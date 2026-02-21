e44a81e8292dfff5c459c9bbdf11a54fd3ac1247
Liam Kirkland <liam.b.kirkland@gmail.com>
2026-02-21 16:39:23 -0500

    update PCA Slider to include non-placeholder images

--- script.js
+++ script.js
@@ -10,19 +10,23 @@ let imgIndex = 0;
 function nextImg() {
   switch (imgIndex) {
     case 0:
-      pcaSlider.src = "./assets/images/testImg1.jpg";
+      pcaSlider.src = "./assets/images/pcaSlide2.jpg";
       imgIndex++;
       break;
     case 1:
-      pcaSlider.src = "./assets/images/testImg2.jpg";
+      pcaSlider.src = "./assets/images/pcaSlide3.jpg";
       imgIndex++;
       break;
     case 2:
-      pcaSlider.src = "./assets/images/testImg3.jpg";
+      pcaSlider.src = "./assets/images/pcaSlide4.jpg";
       imgIndex++;
       break;
     case 3:
-      pcaSlider.src = "./assets/images/PCA2026.jpg";
+      pcaSlider.src = "./assets/images/pcaSlide5.png";
+      imgIndex++;
+      break;
+    case 4:
+      pcaSlider.src = "./assets/images/pcaSlide1.jpg";
       imgIndex = 0;
       break;
   }
@@ -75,4 +79,3 @@ function buttonAction() {
 }
 
 redButton.addEventListener("click", buttonAction);
-//hello world
